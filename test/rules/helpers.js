3a21db24fac542bd435ba8d185a31436317b8460
Mohammed Hussain <2152584+mhhussain@users.noreply.github.com>
2026-05-30 13:19:31 -0400

    Fix helpers.js to use import.meta.url for firestore.rules path

--- test/rules/helpers.js
+++ test/rules/helpers.js
@@ -1,11 +1,15 @@
 import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+import { join, dirname } from "node:path";
 import { initializeTestEnvironment } from "@firebase/rules-unit-testing";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export async function makeEnv() {
   return initializeTestEnvironment({
     projectId: "tapuntap-test",
     firestore: {
-      rules: readFileSync("firestore.rules", "utf8"),
+      rules: readFileSync(join(__dirname, "../../firestore.rules"), "utf8"),
       host: "localhost",
       port: 8080
     }
